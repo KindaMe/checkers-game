80ab1bee7bcfd1f7cad0a6a60f287f50feca768a
Patryk Kalinowski <kalinowski1999@gmail.com>
2021-11-21 13:12:40 +0100

    Fix lots of stuff and clean up code a bit.

--- checkers-game/checkers-game.cpp
+++ checkers-game/checkers-game.cpp
@@ -4,8 +4,7 @@
 #include <string>
 #include <utility>
 
-class square
-{
+class square {
 private:
 	int x = 0;
 	int y = 0;
@@ -81,8 +80,9 @@ public:
 };
 
 void start();
-void switchPlayer(char& player, bool& recentlyScored);
-bool doContinue();
+void switchPlayer(char& player, bool& anotherTarget, bool& selectSet, bool& targetSet);
+void refreshBoard(square field[8][8], char currentPlayer);
+void anotherMove(int& selectX, int& selectY, int& targetX, int& targetY);
 
 int main()
 {
@@ -96,10 +96,12 @@ void start()
 
 	char currentPlayer = 'W';
 
-	bool recentlyScored = false;
+	bool anotherTarget = false;
 
 	std::string select, target;
 	int selectX, selectY, targetX, targetY;
+	bool selectSet = false;
+	bool targetSet = false;
 
 	for (int y = 0; y < 8; y++)
 	{
@@ -111,97 +113,41 @@ void start()
 
 	do
 	{
-		system("cls");
-		//////////////print board
-
-		for (int height = 8 * 4; height >= 0; height--)
-		{
-			if (height % 4 == 0)
-			{
-				for (int width = 0; width <= 8 * 4; width++)
-				{
-					if (width % 4 == 2 && height == 0)
-					{
-						std::cout << " " << (char)((width / 4) + 97) << " ";
-					}
-					else if (width % 4 == 0)
-					{
-						std::cout << "|";
-					}
-					else
-					{
-						std::cout << "---";
-					}
-				}
-			}
-			else if (height % 4 == 2)
-			{
-				for (int width = 0; width <= 8 * 4; width++)
-				{
-					if (width == 0)
-					{
-						std::cout << (height / 4) + 1;
-					}
-					else if (width % 4 == 2)
-					{
-						field[width / 4][height / 4].printDebug();
-					}
-					else if (width % 4 == 0)
-					{
-						std::cout << "|";
-					}
-					else
-					{
-						std::cout << "   ";
-					}
-				}
-			}
-			else
-			{
-				for (int width = 0; width <= 8 * 4; width++)
-				{
-					if (width % 4 == 0)
-					{
-						std::cout << "|";
-					}
-					else
-					{
-						std::cout << "   ";
-					}
-				}
-			}
-
-			std::cout << "\n";
-		}
-
-		std::cout << "\n";
-		std::cout << "\nCurrent player: " << currentPlayer << "\n";
+		refreshBoard(field, currentPlayer);
 
 		//////////////select
 		do
 		{
-			std::cout << "\nSelect pawn: ";
-			getline(std::cin, select);
-			select[0] = tolower(select[0]);
-
-			if ((select[1] > '0' && select[1] <= '8') && (select[0] >= 'a' && select[0] <= 'h'))
+			if (selectSet == false)
 			{
-				selectX = select[0] - 96 - 1;
-				selectY = select[1] - 48 - 1;
+				std::cout << "\nSelect pawn: ";
+				getline(std::cin, select);
+				select[0] = tolower(select[0]);
 
-				if (field[selectX][selectY].getPawn() == currentPlayer)
+				if ((select[1] > '0' && select[1] <= '8') && (select[0] >= 'a' && select[0] <= 'h'))
 				{
-					break;
+					selectX = select[0] - 96 - 1;
+					selectY = select[1] - 48 - 1;
+
+					if (field[selectX][selectY].getPawn() == currentPlayer)
+					{
+						selectSet = true;
+						break;
+					}
+					else
+					{
+						std::cout << "No " << currentPlayer << " pawns in this area.\n";
+						field[selectX][selectY].printDebug();
+					}
 				}
 				else
 				{
-					std::cout << "No " << currentPlayer << " pawns in this area.\n";
-					field[selectX][selectY].printDebug();
+					std::cout << "Invalid input.\n";
 				}
 			}
 			else
 			{
-				std::cout << "Invalid input.\n";
+				break;
 			}
 		} while (true);
 
@@ -221,6 +167,7 @@ void start()
 					if ((targetX - selectX == 1 || targetX - selectX == -1) && (targetY - selectY == 1 || targetY - selectY == -1))//check if trying to move once
 					{
 						std::swap(field[selectX][selectY], field[targetX][targetY]);
+						targetSet = true;
 						break;
 					}
 					else if ((targetX - selectX == 2 || targetX - selectX == -2) && (targetY - selectY == 2 || targetY - selectY == -2))//check if trying to move twice
@@ -229,12 +176,15 @@ void start()
 						{
 							field[(selectX + targetX) / 2][(selectY + targetY) / 2].destroyPawn();
 							std::swap(field[selectX][selectY], field[targetX][targetY]);
+							targetSet = true;
 
 							if ((targetX + 2 < 8 && targetY + 2 < 8) && field[targetX + 1][targetY + 1].getPawn() != ' ' && field[targetX + 1][targetY + 1].getPawn() != currentPlayer)
 							{
 								if (field[targetX + 2][targetY + 2].getPawn() == ' ')
 								{
-									recentlyScored = true;
+									anotherTarget = true;
+									anotherMove(selectX, selectY, targetX, targetY);
+									refreshBoard(field, currentPlayer);
 									break;
 								}
 							}
@@ -242,7 +192,9 @@ void start()
 							{
 								if (field[targetX - 2][targetY - 2].getPawn() == ' ')
 								{
-									recentlyScored = true;
+									anotherTarget = true;
+									anotherMove(selectX, selectY, targetX, targetY);
+									refreshBoard(field, currentPlayer);
 									break;
 								}
 							}
@@ -250,7 +202,9 @@ void start()
 							{
 								if (field[targetX - 2][targetY + 2].getPawn() == ' ')
 								{
-									recentlyScored = true;
+									anotherTarget = true;
+									anotherMove(selectX, selectY, targetX, targetY);
+									refreshBoard(field, currentPlayer);
 									break;
 								}
 							}
@@ -258,7 +212,9 @@ void start()
 							{
 								if (field[targetX + 2][targetY - 2].getPawn() == ' ')
 								{
-									recentlyScored = true;
+									anotherTarget = true;
+									anotherMove(selectX, selectY, targetX, targetY);
+									refreshBoard(field, currentPlayer);
 									break;
 								}
 							}
@@ -266,7 +222,7 @@ void start()
 						}
 						else
 						{
-							std::cout << "No pawn to destroy.\n";
+							std::cout << "Invalid move.\n";
 						}
 					}
 					else
@@ -276,7 +232,7 @@ void start()
 				}
 				else
 				{
-					std::cout << "Field not empty.\n";
+					std::cout << "Invalid move.\n";
 				}
 			}
 			else
@@ -284,22 +240,28 @@ void start()
 				std::cout << "Invalid input.\n";
 			}
 		} while (true);
-		switchPlayer(currentPlayer, recentlyScored);
+		switchPlayer(currentPlayer, anotherTarget, selectSet, targetSet);
 	} while (true);
 }
 
-void switchPlayer(char& player, bool& recentlyScored)
+void switchPlayer(char& player, bool& anotherTarget, bool& selectSet, bool& targetSet)
 {
-	if (recentlyScored == true)
+	if (anotherTarget == true)
 	{
-		if (doContinue() == true)
+		std::cout << "\nAnother attack? (y/n): ";
+		char yn;
+		std::cin >> yn;
+		std::cin.clear();
+		std::cin.ignore();
+		switch (tolower(yn))
 		{
-			recentlyScored = false;
+		case 'y':
+			anotherTarget = false;
+			targetSet = false;
 			return;
-		}
-		else
-		{
-			recentlyScored = false;
+		default:
+			anotherTarget = false;
+			break;
 		}
 	}
 
@@ -314,23 +276,81 @@ void switchPlayer(char& player, bool& recentlyScored)
 	default:
 		exit(2);
 	}
+
+	targetSet = false;
+	selectSet = false;
 }
 
-bool doContinue()
+void refreshBoard(square field[8][8], char currentPlayer)
 {
-	char yn;
-	do
+	system("cls");
+
+	for (int height = 8 * 4; height >= 0; height--)
 	{
-		std::cout << "\nContinue? (y/n): ";
-		std::cin >> yn;
-		switch (tolower(yn))
+		if (height % 4 == 0)
 		{
-		case 'y':
-			return true;
-		case 'n':
-			return false;
-		default:
-			break;
+			for (int width = 0; width <= 8 * 4; width++)
+			{
+				if (width % 4 == 2 && height == 0)
+				{
+					std::cout << " " << (char)((width / 4) + 97) << " ";
+				}
+				else if (width % 4 == 0)
+				{
+					std::cout << "|";
+				}
+				else
+				{
+					std::cout << "---";
+				}
+			}
 		}
-	} while (true);
+		else if (height % 4 == 2)
+		{
+			for (int width = 0; width <= 8 * 4; width++)
+			{
+				if (width == 0)
+				{
+					std::cout << (height / 4) + 1;
+				}
+				else if (width % 4 == 2)
+				{
+					field[width / 4][height / 4].printDebug();
+				}
+				else if (width % 4 == 0)
+				{
+					std::cout << "|";
+				}
+				else
+				{
+					std::cout << "   ";
+				}
+			}
+		}
+		else
+		{
+			for (int width = 0; width <= 8 * 4; width++)
+			{
+				if (width % 4 == 0)
+				{
+					std::cout << "|";
+				}
+				else
+				{
+					std::cout << "   ";
+				}
+			}
+		}
+
+		std::cout << "\n";
+	}
+
+	std::cout << "\n";
+	std::cout << "\nCurrent player: " << currentPlayer << "\n";
+}
+
+void anotherMove(int& selectX, int& selectY, int& targetX, int& targetY)
+{
+	selectX = targetX;
+	selectY = targetY;
 }
